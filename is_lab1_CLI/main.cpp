c7f70017b7bf1d7a0a1e4cb86acb1f11be039d63
Slava <Slava@MADBADRAT-W11>
2023-05-09 14:46:39 +0700

    Add some fixes

--- is_lab1_CLI/main.cpp
+++ is_lab1_CLI/main.cpp
@@ -20,7 +20,18 @@ int main()
 
     for (int i = 0; i < count; i++)
     {
-        if (Products[i]->GetTrustMeasure() >= 0) cout << Products[i]->GetName() << " " << Products[i]->GetTrustMeasure() << endl;
+        if (Products[i]->GetTrustMeasure() >= 0)
+        {
+            cout << "Название банка: " << Products[i]->GetName() << endl;
+            cout << "Сумма кредита: " << Products[i]->GetAmount() << endl;
+            cout << "Процентная ставка: " << Products[i]->GetPercent() << endl;
+            cout << "Срок кредитования: " << Products[i]->GetTerm() << endl;
+            cout << "Цель кредита: " << Products[i]->GetPurpose() << endl;
+            cout << "Ежемесячный платеж: " << Products[i]->GetPayment() << endl;
+            cout << "Размер переплаты: " << Products[i]->GetOverpayment() << endl;
+            cout << "Степень доверия данному предложению: " << Products[i]->GetTrustMeasure() << endl;
+            cout << "------------------------------" << endl;
+        }
     }
 
     system("pause");
